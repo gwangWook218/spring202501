19098e552a8f8b54e54e0fd22665b6c0ff279d52
gwangWook218 <akw2080@naver.com>
2025-01-13 22:11:25 +0900

    250113_mybatis

---
+++ exAKW_mybatisTry/exAKW_mybatisTry/src/main/java/com/example/demo/ExAkwMybatisTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExAkwMybatisTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExAkwMybatisTryApplication.class, args);
+	}
+
+}
